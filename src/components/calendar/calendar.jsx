3b96001212c3740db0dba84342ccc5646c6384a5
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-02-20 17:19:20 -0800

    started building the calender and cells

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,9 +1,28 @@
+import { CalendarCell } from "../calendarCell/calendarCell"
+import './calendar.css'
+
 export const Calendar = () => {
+  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  
+
   return (
-    <section>
-      <div>
-        <h1>Calendar</h1>
-      </div>
-    </section>
+    <table id='calendar'>
+      <thead>
+        <tr>
+          {weekDays.map(day => <th key={day} className={day}>{day}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <CalendarCell />
+          <CalendarCell />
+          <CalendarCell />
+          <CalendarCell />
+          <CalendarCell />
+          <CalendarCell />
+          <CalendarCell />
+        </tr>
+      </tbody>
+    </table>
   )
 }
---
+++ src/components/calendarCell/calendarCell.jsx
@@ -0,0 +1,9 @@
+import './calendarCell.css'
+
+export const CalendarCell = () => {
+  return (
+    <td className='calendarCellContainer'>
+      <div className='calendarCell'>1</div>
+    </td>
+  )
+}
